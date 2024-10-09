374f9247ec8d1c0f3cae15a2f760c8d99c47aad1
1cbyc <isaacnsisong@gmail.com>
2024-10-09 20:40:39 +0100

    building a youtube downloader that works

--- app/routes.py
+++ app/routes.py
@@ -32,7 +32,7 @@ def index():
             # yt = YouTube(video_url) # assuming i did not clean the url
             yt = YouTube(cleaned_url)  # will use the cleaned URL instead of video_url
             # video = yt.streams.get_highest_resolution()
-            video = yt.streams.filter(progressive=True, file_extension='mp4').first()  # Select the first available stream
+            video = yt.streams.filter(progressive=True, file_extension='mp4').first()  # will select the first available stream instead
             video.download(output_path=DOWNLOAD_FOLDER)
             file_path = os.path.join(DOWNLOAD_FOLDER, yt.title + ".mp4")
             return send_file(file_path, as_attachment=True)
